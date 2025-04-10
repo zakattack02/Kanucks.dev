1d6925cd5d157fde9fb02beaf85459a400a70627
Zak Konik <zak@zmbg.us>
2025-04-10 12:32:21 -0400

    Refactor Main component to Home, add Contact page with social media links, and update styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Main from './pages/Main.jsx'
+import Main from './pages/Home.jsx'
 import Nav from './components/Navbar.jsx'
+import Contact from './pages/Contact.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Main />
+      <Main />
+    <div style={{ paddingLeft: '15%  ' }} >
+      <Contact />
+    </div>
   </StrictMode>,
 )
  
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { motion } from "framer-motion";
+
+export default function Main() {
+  return (
+    <div className="bg-[#1a1c1d] text-[#eeeeee] min-h-screen flex flex-col justify-center items-center font-[Arvo] text-center px-[12.5vw] w-[75vw] mx-auto">
+      <motion.h1
+        className="text-6xl font-bold mb-6"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        <h2>Zak Konik</h2>
+      </motion.h1>
+
+    </div>
+  );
+}
