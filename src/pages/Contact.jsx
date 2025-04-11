a30a32a118425aeb650e6ee837ee9500f03e57eb
Zak Konik <zak@zmbg.us>
2025-04-10 20:57:57 -0400

    refactor HTML structure: adjust body styles and navigation padding

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,21 +7,21 @@ const Contact = () => {
     <span className="flex justify-center items-center gap-6 mt-2">
     <a href="https://github.com/zakattack02/" aria-label="GitHub">
       <img height={100} width={100}
-        src="/github-mark-white.png"
+        src="/github-mark-white.svg"
         alt="GitHub"
         className="w-8 h-8"
       />
     </a>
     <a href="https://www.linkedin.com/in/zachary-konik/" aria-label="LinkedIn">
       <img height={100} width={100}
-        src="/icons8-linkedin-100.png"
+        src="/linkedin-white.svg"
         alt="LinkedIn"
         className="w-8 h-8"
       />
     </a>
     <a href="mailto:zak@zmbg.us" aria-label="Email">
       <img height={100} width={100}
-        src="/icons8-email-100.png"
+        src="/email-white.png"
         alt="Email"
         className="w-8 h-8"
       />
