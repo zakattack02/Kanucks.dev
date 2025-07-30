066611f6c4b392166a9b67bf2f80eab9f5b8061f
Zak Konik <zak@zmbg.us>
2025-07-30 12:12:14 -0400

    Rearanged location of elements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import "../index.css";
 
 export default function Main() {
   return (
-    <div className="text-[#eeeeee] min-h-screen w-full" style={{ backgroundColor: "#1a1c1d" }}>
+    <div className="text-[#eeeeee] min-h-screen w-full">
       {/* Content - centered on page */}
       <div className="flex flex-col items-center justify-center min-h-screen space-y-8">
         <motion.h1
@@ -21,31 +21,6 @@ export default function Main() {
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.3 }}
         >
-              <div>
-    <span className="flex justify-center items-center gap-6 mt-2">
-    <a href="https://github.com/zakattack02/" aria-label="GitHub">
-      <img height={100} width={100}
-        src="/github-mark-white.png"
-        alt="GitHub"
-        className="w-8 h-8"
-      />
-    </a>
-    <a href="https://www.linkedin.com/in/zachary-konik/" aria-label="LinkedIn">
-      <img height={100} width={100}
-        src="/icons8-linkedin-100.png"
-        alt="LinkedIn"
-        className="w-8 h-8"
-      />
-    </a>
-    <a href="mailto:zak@zmbg.us" aria-label="Email">
-      <img height={100} width={100}
-        src="/icons8-email-100.png"
-        alt="Email"
-        className="w-8 h-8"
-      />
-    </a>
-  </span>
-  </div>
         </motion.div>
       </div>
     </div>
